b73b07680e72dbfcca9ec506b6e198a4d0de16ac
Your Name <tylermack5@gmail.com>
2010-06-09 09:42:27 -0700

    Word length validation WIP. ComputerLogin# added to stylesheet

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,7 +4,9 @@ class Quote < ActiveRecord::Base
   validates_length_of :content, :maximum => 450, :message => "is too long"
   #validates_format_of :content, :allow_blank => true, :with => /idunno lol/, :message => "contains a word that is too long"
   validates_format_of :name, :with => /^.+\s.+$/, :message => "must contain both first and last name"
+  validates_numericality_of :login_id, :only_integer => true, :on => :create
   validates_numericality_of :student_id, :only_integer => true, :on => :create
+  validates_length_of :login_id, :is => 7, :message => "ID is the wrong length (should be 7 characters)"
   validates_length_of :student_id, :is => 6, :message => "ID is the wrong length (should be 6 characters)"
   validates_uniqueness_of :student_id, :scope => :student_id, :message => "ID has already been used"
   validates_format_of :email, :with => /^.+@.+\.(com|org|net|edu)$|^$/
