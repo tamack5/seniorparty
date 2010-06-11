85dd0095f68e57bf5c1a05463ec2eddb9c879ed8
Your Name <tylermack5@gmail.com>
2010-06-10 21:39:54 -0700

    Changed password for http_basic

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -101,7 +101,7 @@ end
 
   def authentication
     id = "admin"
-    pass = "admin"
+    pass = "sr2010grad"
     authenticate_or_request_with_http_basic do |user,password|
       user == id && password == pass
     end
